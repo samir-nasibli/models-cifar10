f3599127a8cdbecf9786b4765685dbe5fd785479
Samir Nasibli <alrightalrightalright100@gmail.com>
2020-06-04 22:19:13 +0300

    update tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,6 +4,14 @@ from utils import common_utils
 
 
 def test_merge_two_dicts():
+    a = {'a' : 1, 'b' : 2, 'c' : 3}
+    b = {'b' : 2, 'd' : 4}
+    expected_c = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+    c =  merge_two_dicts(a,b)
+    assert c == expected_c
+
+
+def test_merge_two_dicts_return_type():
     a = {}
     b = {}
     c =  common_utils.merge_two_dicts(a,b)
