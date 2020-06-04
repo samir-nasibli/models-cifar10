431fa7f54d5e6aa60287f06340e5c7af4f8da5c8
SamirNasibli <samir.nasibli@intel.com>
2020-06-04 07:25:00 +0300

    Update common_utils.py

--- utils/common_utils.py
+++ utils/common_utils.py
@@ -1,16 +1,19 @@
-
+import torch
 
 def merge_two_dicts(x, y):
-	"""
-	Merges x and y
-	"""
+    """
+    Merges x and y
+    """
     z = x.copy()   # start with x's keys and values
     z.update(y)    # modifies z with y's keys and values & returns None
     return z
 
 
 def print_cuda_device_info():
-	print(torch.__version__)
+    """
+    Prints some initial cuda device info
+    """
+    print(torch.__version__)
     print(torch.cuda.current_device())
     print(torch.cuda.device(0))
     print(torch.cuda.device_count())
